fb4be0f9c0fdc931a22fdfa0fd2e248f3cb38f17
KarloVrd <karlo.vrdoljak@gmail.com>
2023-04-11 21:44:29 +0200

    bug fix
    super_user can now be created

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,3 +6,28 @@ urlpatterns = [
     path('token/', views.TokenViews.as_view(), name='get_token'),
     path('entrance-key/', views.create_entrance_key, name='create_entrance_key'),
 ]
+
+'''
+root = https://karlo13.pythonanywhere.com/
+
+/accounts/entrance-key
+	POST:
+        int group_id        - opcinalno
+		datetime expires_at - opcinalno
+		int uses_left       - opcinalno
+		-> returns entrance_code
+
+/accounts/user
+	POST:
+		string email
+		string password
+		string username     - opcinalno
+		string entrance_code - opcinalno
+		-> returns token
+
+/accounts/token
+	POST:
+		string username / email
+		string password
+		-> returns token
+'''
--- testRequest.py
+++ testRequest.py
@@ -1,23 +1,26 @@
 import requests, json
 
 # post new entrance key
-url = 'https://karlo13.pythonanywhere.com/accounts/entrance_key/'
 url = 'http://127.0.0.1:8000/accounts/entrance-key/'
+url = 'https://karlo13.pythonanywhere.com/accounts/entrance-key/'
 
 data = {'expires_at': '2024-12-31T23:59:59Z', 'uses_left': 5}
+data = {'email': 'test@example.com', 'password': 'testpassword'}
+
 header = {'content-type': 'application/json'}
 response = requests.post(url, data=json.dumps(data), headers=header)
 print(response.status_code)
 print(response.content)
 
-code = response.json()['entrance_code']
 
-url = 'https://karlo13.pythonanywhere.com/accounts/user/'
-url = 'http://127.0.0.1:8000/accounts/user/'
+# code = response.json()['entrance_code']
 
-data = {'username': 'testuser', 'password': 'testpassword', 'email': 'test4@example.com', 'entrance_code': code}
-header = {'content-type': 'application/json'}
-response = requests.post(url, data=json.dumps(data), headers=header)
+# url = 'https://karlo13.pythonanywhere.com/accounts/user/'
+# url = 'http://127.0.0.1:8000/accounts/user/'
 
-print(response.status_code)
-print(response.content)
+# data = {'username': 'testuser', 'password': 'testpassword', 'email': 'test4@example.com', 'entrance_code': code}
+# header = {'content-type': 'application/json'}
+# response = requests.post(url, data=json.dumps(data), headers=header)
+
+# print(response.status_code)
+# print(response.content)
